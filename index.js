979d4136c8423a094f944e091eccb650a80f7ddb
Dan Kaplun <dbkaplun@gmail.com>
2015-06-27 17:44:39 -0400

    more columns

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-var path = require('path');
 var childProcess = require('child_process');
 var tasklist = require('tasklist');
 var parseColumns = require('parse-columns');
@@ -12,40 +11,42 @@ function win(cb) {
 		}
 
 		var ret = data.map(function (el) {
-			return {
-				pid: el.pid,
-				name: el.imageName,
-				cmd: el.imageName
-			};
+			el.name = el.cmd = el.imageName;
+			return el;
 		});
 
 		cb(null, ret);
 	});
 }
 
+// https://www.freebsd.org/cgi/man.cgi?query=ps(1)&sektion=#KEYWORDS
+def.cols = [
+	{o: 'pid',  header: 'pid',  transform: Number},
+	{o: 'ppid', header: 'ppid', transform: Number},
+	{o: 'user', header: 'user'},
+	{o: 'comm', header: 'name'},
+	{o: 'args', header: 'cmd'},
+	{o: '%cpu', header: 'cpu'},
+	{o: '%mem', header: 'mem'},
+];
+def.cols.byHeader = def.cols.reduce(function (colsByHeader, col) {
+	colsByHeader[col.header] = col;
+	return colsByHeader;
+}, {});
+
 function def(cb) {
-	childProcess.execFile('ps', ['axo', 'pid,comm,args'], function (err, stdout) {
+	var os = def.cols.map(function (col) { return col.o; }).join(',');
+	childProcess.execFile('ps', ['axo', os], function (err, stdout) {
 		if (err) {
 			cb(err);
 			return;
 		}
 
 		var ret = parseColumns(stdout, {
-			headers: [
-				'pid',
-				'name',
-				'cmd'
-			],
+			headers: def.cols.map(function (col) { return col.header; }),
 			transform: function (el, header) {
-				if (header === 'pid') {
-					return Number(el);
-				}
-
-				if (header === 'name') {
-					return path.basename(el);
-				}
-
-				return el;
+				var transform = def.cols.byHeader[header].transform;
+				return transform ? transform(el) : el;
 			}
 		});
 
