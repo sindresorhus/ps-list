0c2f2fc7b32b73a3b7e8ba7329f20dac144bebe6
Andrea Parodi <andrea@parro.it>
2016-03-03 09:18:51 +0100

    Removed String.includes usage

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ test(async t => {
 	const binName = process.platform === 'win32' ? 'node.exe' : 'ava';
 	const list = await fn();
 
-	t.true(list.some(x => x.name.includes(binName)));
+	t.true(list.some(x => x.name.indexOf(binName) !== -1));
 	t.true(list.every(x =>
 		typeof x.pid === 'number' &&
 		typeof x.name === 'string' &&
