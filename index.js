16957f6774f1a4d8242b135f030ee771bb30949b
will <667@june07.com>
2018-07-31 21:47:05 -0700

    Updated pull https://github.com/sindresorhus/ps-list/pull/16

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const childProcess = require('child_process');
 const tasklist = require('tasklist');
 const pify = require('pify');
+const mypid = require('process').pid;
 
 function win() {
 	return tasklist().then(data => {
@@ -19,6 +20,7 @@ function win() {
 function def(options = {}) {
 	const ret = {};
 	const flags = (options.all === false ? '' : 'a') + 'wwxo';
+	const includeSelf = (options.includeSelf === false ? false: true)
 
 	return Promise.all(['comm', 'args', 'ppid', '%cpu', '%mem'].map(cmd => {
 		return pify(childProcess.execFile)('ps', [flags, `pid,${cmd}`]).then(stdout => {
@@ -38,16 +40,29 @@ function def(options = {}) {
 		// Filter out inconsistencies as there might be race
 		// issues due to differences in `ps` between the spawns
 		// TODO: Use `Object.entries` when targeting Node.js 8
-		return Object.keys(ret).filter(x => ret[x].comm && ret[x].args && ret[x].ppid && ret[x]['%cpu'] && ret[x]['%mem']).map(x => {
-			return {
-				pid: Number.parseInt(x, 10),
-				name: path.basename(ret[x].comm),
-				cmd: ret[x].args,
-				ppid: Number.parseInt(ret[x].ppid, 10),
-				cpu: Number.parseFloat(ret[x]['%cpu']),
-				memory: Number.parseFloat(ret[x]['%mem'])
-			};
-		});
+		if (includeSelf) {
+			return Object.keys(ret).filter(x => ret[x].comm && ret[x].args && ret[x].ppid && ret[x]['%cpu'] && ret[x]['%mem']).map(x => {
+				return {
+					pid: Number.parseInt(x, 10),
+					name: path.basename(ret[x].comm),
+					cmd: ret[x].args,
+					ppid: Number.parseInt(ret[x].ppid, 10),
+					cpu: Number.parseFloat(ret[x]['%cpu']),
+					memory: Number.parseFloat(ret[x]['%mem'])
+				};
+			});
+		} else {
+			return Object.keys(ret).filter(x => ret[x].comm && ret[x].args && ret[x].ppid && ret[x]['%cpu'] && ret[x]['%mem']).filter(x => parseInt(x, 10) !== mypid).map(x => {
+				return {
+					pid: Number.parseInt(x, 10),
+					name: path.basename(ret[x].comm),
+					cmd: ret[x].args,
+					ppid: Number.parseInt(ret[x].ppid, 10),
+					cpu: Number.parseFloat(ret[x]['%cpu']),
+					memory: Number.parseFloat(ret[x]['%mem'])
+				};
+			});
+		}
 	});
 }
 
