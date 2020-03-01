7d79425d61b31435277f6b73531909f16e721f96
Vano Devium <webdevium@gmail.com>
2020-03-01 13:02:54 +0200

    Changed order of imports in test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import childProcess from 'child_process';
-import psList from '.';
 import test from 'ava';
+import psList from '.';
 
 const isWindows = process.platform === 'win32';
 const nodeBinaryName = isWindows ? 'node.exe' : 'node';
